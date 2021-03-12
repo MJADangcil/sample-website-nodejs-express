e4e41ed11c95b43d9f9ffc3b2341a96877bc3c9a
Michael Josh Dangcil <michaeljoshdangcil@gmail.com>
2021-03-11 18:03:13 -0800

    Stores user feedback and display in recent feedback

--- routes/feedback.js
+++ routes/feedback.js
@@ -11,6 +11,8 @@ module.exports = params => {
       const feedback = await feedbackService.getList();
 
       const errors = request.session.feedback ? request.session.feedback.errors : false;
+      const successMessage = request.session.feedback ? request.session.feedback.message : false;
+
       request.session.feedback = {};
 
       return response.render('layout', {
@@ -18,6 +20,7 @@ module.exports = params => {
         template: 'feedback',
         feedback,
         errors,
+        successMessage,
       });
     } catch (err) {
       return next(err);
@@ -51,7 +54,7 @@ module.exports = params => {
         .escape()
         .withMessage('A message is required'),
     ],
-    (request, response) => {
+    async (request, response) => {
       const errors = validationResult(request);
 
       if (!errors.isEmpty()) {
@@ -60,7 +63,13 @@ module.exports = params => {
         };
         return response.redirect('/feedback');
       }
-      return response.send('Feedback form posted');
+
+      const { name, email, title, message } = request.body;
+      await feedbackService.addEntry(name, email, title, message);
+      request.session.feedback = {
+        message: 'Thank you for your feedback!',
+      };
+      return response.redirect('/feedback');
     }
   );
 
